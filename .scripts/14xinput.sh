ac28800e317c9f3e1ecca95afdc4af2f8ebc2b23
Pawel Krolikowski <pawel.krolikowski@geneity.co.uk>
2014-10-31 00:30:17 +0100

    More on xinput.

--- .scripts/14xinput.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-xinput set-prop 14 "Device Accel Constant Deceleration" 3.8
-xinput set-prop 14 "Device Accel Velocity Scaling" 1
-xinput set-prop 14 "Device Accel Profile" -1
-
-xset m 1 10000
-
-
